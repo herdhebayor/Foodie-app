b84e98bc99da34d45aeef1316b39ae7bfaed997d
herdhebayor <herdhebayor22@gmail.com>
2026-01-07 22:13:13 +0100

    second commit

--- components/ProductListCard.jsx
+++ components/ProductListCard.jsx
@@ -1,1 +1,30 @@
-import React from ''
+import React from 'react'
+
+
+function ListCard ({product}){
+
+
+    return (
+			<>
+				<section className='py-6 px-3 block border  border-b-gray-600 '>
+					<div className='flex h-50'>
+						<img
+							className='w-50 h-full rounded-md'
+							src={product.image}
+							alt={product.name}
+						/>
+						<div className='flex w-full h-full justify-between '>
+							<div>
+								<h3 className='text-2xl font-semibold'>{product.name}</h3>
+								<p>{product.description}</p>
+							</div>
+							<div className='flex flex-col justify-between'>
+								<h3 className='text-2xl font-semibold'>#{product.price}</h3>
+								<button>add to cart</button>
+							</div>
+						</div>
+					</div>
+				</section>
+			</>
+		)
+}
